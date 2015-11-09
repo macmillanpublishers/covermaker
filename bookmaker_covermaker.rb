96ef80038a625b70de78d9e3d8c0e9d8b3ab15a4
nelliemckesson <nellie.mckesson@macmillan.com>
2015-11-09 13:00:05 -0500

    troubleshooting cover stuff

--- bookmaker_covermaker.rb
+++ bookmaker_covermaker.rb
@@ -67,6 +67,11 @@ end
 # inserts the css into the head of the html
 pdf_html = File.read("#{template_html}").to_s.gsub(/CSSFILEHERE/,"#{css_file}").gsub(/BOOKTITLE/,"#{book_title}").gsub(/BOOKSUBTITLE/,"#{book_subtitle}").gsub(/BOOKAUTHOR/,"#{book_author}")
 
+test_cover_html = File.join(coverdir, "cover.html")
+File.open(test_cover_html, "w") do |cover|
+  cover.write pdf_html
+end
+
 # sends file to docraptor for conversion
 # currently running in test mode; remove test when css is finalized
 cover_pdf = File.join(coverdir, "cover.pdf")
