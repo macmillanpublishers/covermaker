d2ab11874ba8142c839064c23991f9f580707e9e
nelliemckesson <nellie.mckesson@macmillan.com>
2016-04-07 18:28:33 -0400

    tweaking colorspace

--- bookmaker_covermaker.rb
+++ bookmaker_covermaker.rb
@@ -112,8 +112,8 @@ if File.file?(final_cover) or File.file?(archived_cover)
   shave = (currwidth - targetwidth) / 2
   puts targetcolor
   FileUtils.cp(cover_js_file, pdf_js_file)
-  `convert "#{watermarktmp}" -shave '#{shave}x0' -quality 100 -colorspace #{targetcolor} "#{watermarktmp}"`
-  `convert "#{watermarktmp}" "#{currcover}" -append -colorspace #{targetcolor} "#{currcover}"`
+  `convert "#{watermarktmp}" -shave '#{shave}x0' -quality 100 "#{watermarktmp}"`
+  `convert "#{watermarktmp}" "#{currcover}" -append "#{currcover}"`
   #FileUtils.rm(watermarktmp)
 else
   # sends file to docraptor for conversion
