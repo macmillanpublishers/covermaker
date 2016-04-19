2f2a2c2bb6982bfc972118145cc5c7c9898799cc
nelliemckesson <nellie.mckesson@macmillan.com>
2016-04-19 10:19:36 -0400

    tweaking anthology styles

--- scripts/generic/get_titlepage.js
+++ scripts/generic/get_titlepage.js
@@ -8,12 +8,16 @@ fs.readFile(file, function editContent (err, contents) {
           xmlMode: true
         });
 
+  if ($('.TitlepageBookSubtitlestit + .TitlepageAuthorNameau').length > 1 ) {
+    $('section[data-type="titlepage"]').addClass("anthology");
+  };
+
   var content = $('section[data-type="titlepage"]');
   var $body = $( '<body></body>' );
   $body.append(content)
   var $head = $( '<head></head>' );
   $head.append( $('<title>Generated Titlepage</title>') );
-  var output = $('html').empty().append( $head, $body )
+  var output = $('html').empty().append( $head, $body );
     fs.writeFile(newfile, output, function(err) {
       if(err) {
           return console.log(err);
